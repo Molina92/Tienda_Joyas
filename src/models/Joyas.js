89da8649c2d5e35ab97693ee8211c36adcefd3a6
Daniel Molina <danielmolina9202@gmail.com>
2024-12-19 08:28:36 -0300

    Estructura de datos Hateoas añadida

--- src/models/Joyas.js
+++ src/models/Joyas.js
@@ -1,15 +1,23 @@
-const { DB } = require("../config/db")
-const format = require("pg-format")
+const { DB } = require("../config/db");
+const format = require("pg-format");
+
+const prepareHATEOAS = (joyas) => {
+    const results = joyas.map((j) => ({
+        name: j.nombre,
+        href: `/joyas/${j.id}`,
+    }));
+    const total = joyas.length;
+    return { total, results };
+};
 
 const getJoyas = async ({ limit = 10, order_by = "id_ASC", page = 1 }) => {
     try {
-
-        const [campo, direccion] = order_by.split("_")
-        const offset = Math.abs((page - 1) * limit)
+        const [campo, direccion] = order_by.split("_");
+        const offset = Math.abs((page - 1) * limit);
 
         const SQLQuery = format(`
             SELECT * FROM inventario
-            order by %s %s 
+            ORDER BY %s %s 
             LIMIT %s
             OFFSET %s`,
             campo,
@@ -20,31 +28,33 @@ const getJoyas = async ({ limit = 10, order_by = "id_ASC", page = 1 }) => {
 
         const SQLSubQueryStock = format(` 
             SELECT SUM(stock) AS total_stock FROM 
-            ( SELECT stock FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s )`,
+            (SELECT stock FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s)`,
             campo,
             direccion,
             limit,
             offset
         );
 
-        const { rows: results, rowCount: TotalJoyas } = await DB.query(SQLQuery)
-        const { rowCount: count } = await DB.query('SELECT * FROM inventario')
-        const result = await DB.query(SQLSubQueryStock)
-        const stockTotal = result.rows[0].total_stock
+        const { rows: results, rowCount: totalJoyas } = await DB.query(SQLQuery);
+        const { rowCount: count } = await DB.query('SELECT * FROM inventario');
+        const result = await DB.query(SQLSubQueryStock);
+        const stockTotal = result.rows[0].total_stock;
+
+        // Preparar respuesta según estructura de datos HATEOAS
+        const HATEOAS = prepareHATEOAS(results);
 
         return {
-            TotalJoyas,
+            totalJoyas,
             stockTotal,
-            results,
-            pages: Math.ceil(count / limit)
-        }
-
+            pages: Math.ceil(count / limit),
+            HATEOAS
+        };
 
     } catch (error) {
-        throw error
+        throw error;
     }
-}
+};
 
 module.exports = {
     getJoyas
-}
+};
